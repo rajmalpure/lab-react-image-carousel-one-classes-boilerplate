c73418fe32a43674a23d53f2c915d376ed137d26
rajmalpure <malpure.raj@kalvium.community>
2024-02-15 16:00:47 +0530

    added jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,6 +7,60 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+    constructor(){
+        super();
+        this.state = {
+            index:0
+        }
+    }
+
+    render(){
+        let {index} = this.state; //destruction
+        let currentImage = images[index];
+
+        let handlePrev = ()=>{
+            this.setState({index:(this.state.index-1+images.length)%images.length})
+        }
+
+        let handleNext = ()=>{
+            this.setState({index:(this.state.index+1)%images.length})
+        }
+        
+        return(
+            <>
+            <div className = "mainContainer">
+                <div>
+                    <ArrowBackIosIcon onClick = {handlePrev}/>
+                </div>
+
+                <div className="imageBox">
+                    <img src={currentImage.img} alt="" />
+
+                </div>
+
+                <div className="imageContainer">
+
+                    <div className="titleBox">
+                        <p>{currentImage.title}</p>
+
+                    </div>
+
+                    <div className="subTitleBox">
+                        <p>{currentImage.subtitle}</p>
+
+                    </div>
+
+                </div>
+
+                <div>
+                    <ArrowForwardIosIcon onClick = {handleNext}/>
+                </div>
+
+                
+            </div>
+            </>
+        )
+    }
   
 }
 
